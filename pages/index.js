0ec1110987d55120745f9b05fae7b8c175a66c4d
julian-solisaguilar <55472888+julian-solisaguilar@users.noreply.github.com>
2022-07-27 16:11:14 -0600

    Added the visible cans with their proper amounts and prices

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCartOutlined";
 import SubmitForm from "../components/SubmitForm";
 import SubmitButton from "../components/SubmitButton";
 import ClearButton from "../components/ClearButton";
+import Product from "../components/Product";
 
 const CartItem = styled(ListItem)({
   width: `260px`,
@@ -24,10 +25,10 @@ export default function Home() {
   const [amount, setAmount] = useState(0);
   const [cart, setCart] = useState([]);
   const [cans, setCans] = useState([
-    { soda: "CocaCola", amount: 10, price: 500 },
-    { soda: "Pepsi", amount: 8, price: 600 },
-    { soda: "Fanta", amount: 10, price: 550 },
-    { soda: "Sprite", amount: 15, price: 725 },
+    { soda: "CocaCola", amount: 10, price: 500, src: "CocaCola.svg" },
+    { soda: "Pepsi", amount: 8, price: 600, src: "Pepsi.svg" },
+    { soda: "Fanta", amount: 10, price: 550, src: "Fanta.svg" },
+    { soda: "Sprite", amount: 15, price: 725, src: "Sprite.svg" },
   ]);
   const [coins, setCoins] = useState([
     { value: 500, amount: 20 },
@@ -37,7 +38,27 @@ export default function Home() {
   ]);
 
   useEffect(() => {
-    console.log(cart);
+    let key = cart.length;
+    let canToSubstract = "";
+    let amountToSubstract = 0;
+    cart.find((item) => {
+      if (key === item.key) {
+        canToSubstract = item.soda;
+        amountToSubstract = item.amount;
+      }
+    });
+    console.log(canToSubstract, amountToSubstract);
+    //update cans amount
+    setCans((prevCans) =>
+      prevCans.map((can) => {
+        if (can.soda === canToSubstract) {
+          can.amount = can.amount - amountToSubstract;
+        }
+        return can;
+      }
+      )
+    );
+    console.log(cans);
   }, [cart]);
 
   const getItems = () => {
@@ -75,7 +96,10 @@ export default function Home() {
           price = can.price;
         }
       });
-      setCart([...cart, { soda: soda, amount: amount, price: price }]);
+      setCart([
+        ...cart,
+        { soda: soda, amount: amount, price: price, key: cart.length },
+      ]);
     }
   };
 
@@ -88,14 +112,29 @@ export default function Home() {
     setCart([]);
   };
 
+  const getProducts = () => {
+    return cans.map((can, index) => {
+      return (
+        <Product
+          key={index}
+          image={can.src}
+          name={can.soda}
+          amount={can.amount}
+          price={can.price}
+        />
+      );
+    });
+  };
+
   return (
     <>
       <div className={styles.Title}>
-        <h1 className={styles.Title_header}>
+        <h1 className={styles.TitleHeader}>
           EXAMEN 2 - INGENIERIA DE SOFTWARE - PARTE 2 - B97634
         </h1>
       </div>
       <div className={styles.Container}>
+        <div className={styles.Cans}>{getProducts()}</div>
         <div className={styles.Machine}>
           <img src="../maquinita_1.png" alt="machine" />
           <div className={styles.Inputs}>
@@ -103,6 +142,7 @@ export default function Home() {
               addToCart={addToCart}
               setAmount={setAmount}
               setSoda={setSoda}
+              value={soda}
             />
           </div>
         </div>
