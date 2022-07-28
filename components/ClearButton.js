d09e096c050ebbf54733385bc0a1da43c712a371
julian-solisaguilar <55472888+julian-solisaguilar@users.noreply.github.com>
2022-07-27 19:41:25 -0600

    Added Tooltip for the clear button

--- components/ClearButton.js
+++ components/ClearButton.js
@@ -1,24 +1,27 @@
 import { Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { Tooltip } from "@mui/material";
 
 const ActionButton = styled(Button)({
   height: `42px`,
   margin: `0px`,
 });
 
-export default function SubmitButton(props) {
+export default function ClearButton(props) {
   return (
     <>
-      <ActionButton
-        variant="contained"
-        size="large"
-        color={props.color}
-        type="submit"
-        onClick={props.onClick}
-      >
-        <DeleteIcon />
-      </ActionButton>
+      <Tooltip title="Vaciar Carrito" arrow placement="bottom">
+        <ActionButton
+          variant="contained"
+          size="large"
+          color={props.color}
+          type="submit"
+          onClick={props.onClick}
+        >
+          <DeleteIcon />
+        </ActionButton>
+      </Tooltip>
     </>
   );
 }
